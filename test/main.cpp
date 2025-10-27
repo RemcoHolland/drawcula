595ca91c96bd4f497b3d8ace52b6512b918e5718
Remco Holland <remco_holland@hotmail.com>
2025-10-27 23:19:53 +0100

    Simplified main

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,8 @@
 #include <gtest/gtest.h>
-#include <chrono>
-#include <cassert> 
 #include "magicmoves.h"
 
-int main(int argc, char** argv) {
-	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-
-	::testing::InitGoogleTest(&argc, argv);
+int main(int argc, char **argv) {
 	initmagicmoves();
-	int result = RUN_ALL_TESTS();
-
-	// current performance for perft 5 startposition: ~ 8,000,000 nps in x64-Debug mode
-	long long testtime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - startTime).count();
-	assert(testtime < 10000); //Total testing time must be lower than 10 seconds
-	return result;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
